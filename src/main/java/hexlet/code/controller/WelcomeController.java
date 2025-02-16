6424659cce0a224fd3ff64096b13147b382fe74b
Dmitry Konstantinov <dm3ypost@gmail.com>
2025-02-16 14:53:38 +0300

    Add /controller/WelcomeController

--- src/main/java/hexlet/code/controller/WelcomeController.java
+++ src/main/java/hexlet/code/controller/WelcomeController.java
@@ -1,2 +1,12 @@
-package hexlet.code.controller;public class WelcomeController {
+package hexlet.code.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class WelcomeController {
+    @GetMapping("/welcome")
+    public String root() {
+        return "Welcome to Spring";
+    }
 }
