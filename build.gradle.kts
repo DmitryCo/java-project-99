63385afb4ce354d65b927b81b861bc2a2fc970df
Dmitry Konstantinov <dm3ypost@gmail.com>
2025-02-16 14:57:49 +0300

    Create a new project

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+	id("java")
+	id("application")
+	id("checkstyle")
+	id("jacoco")
+	id("org.springframework.boot") version "3.4.2"
+	id("io.spring.dependency-management") version "1.1.7"
+	id("io.freefair.lombok") version "8.6"
+}
+
+group = "hexlet.code"
+version = "0.0.1-SNAPSHOT"
+
+application {
+	mainClass = "hexlet.code.AppApplication"
+}
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-devtools")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	finalizedBy(tasks.jacocoTestReport)
+	useJUnitPlatform()
+}
+
+tasks.jacocoTestReport {
+	reports {
+		xml.required.set(true)
+	}
+}
