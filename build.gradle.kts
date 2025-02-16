99c3f3abff5f0f3cfe74f2e43e5c8ef0955656f5
Dmitry Konstantinov <dm3ypost@gmail.com>
2025-02-16 20:15:34 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,9 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-devtools")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	runtimeOnly("com.h2database:h2:2.2.224")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
