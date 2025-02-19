2c535043e42ddb82cc8b7fcce83563033617ff2a
Dmitry Konstantinov <dm3ypost@gmail.com>
2025-02-20 00:53:23 +0300

    Add /dto/user/UserCreateDTO; UserDTO; UserUpdateDTO

--- src/main/java/hexlet/code/dto/user/UserCreateDTO.java
+++ src/main/java/hexlet/code/dto/user/UserCreateDTO.java
@@ -1,2 +1,26 @@
-package hexlet.code.dto.user;public class UserCreateDTO {
+package hexlet.code.dto.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class UserCreateDTO {
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    @Size(min = 3)
+    private String password;
 }
--- src/main/java/hexlet/code/dto/user/UserDTO.java
+++ src/main/java/hexlet/code/dto/user/UserDTO.java
@@ -1,2 +1,25 @@
-package hexlet.code.dto.user;public class UserDTO {
+package hexlet.code.dto.user;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Setter
+@Getter
+public class UserDTO {
+
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String firstName;
+
+    private String lastName;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private LocalDate createdAt;
 }
