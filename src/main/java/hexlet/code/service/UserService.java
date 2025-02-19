fce64c1f4f1f497a1cab70a209d16d802da1a42c
Dmitry Konstantinov <dm3ypost@gmail.com>
2025-02-20 00:58:34 +0300

    Add /service/CustomUserDetailsService; UserService

--- src/main/java/hexlet/code/service/UserService.java
+++ src/main/java/hexlet/code/service/UserService.java
@@ -1,2 +1,28 @@
-package hexlet.code.service;public class UserService {
+package hexlet.code.service;
+
+import hexlet.code.dto.user.UserCreateDTO;
+import hexlet.code.dto.user.UserDTO;
+import hexlet.code.mapper.UserMapper;
+import hexlet.code.repository.UserRepository;
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    public UserDTO create(UserCreateDTO userData) {
+        var user = userMapper.map(userData);
+        userRepository.save(user);
+        return userMapper.map(user);
+    }
 }
