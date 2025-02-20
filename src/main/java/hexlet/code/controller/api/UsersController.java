5b1a8d003093259ac971802e4e21b27e5cd636d7
Dmitry Konstantinov <dm3ypost@gmail.com>
2025-02-20 23:00:20 +0300

    Add @PreAuthorize()

--- src/main/java/hexlet/code/controller/api/UsersController.java
+++ src/main/java/hexlet/code/controller/api/UsersController.java
@@ -13,13 +13,28 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
 public class UsersController {
+
+    private static final String ONLY_OWNER_OR_ADMIN = """
+                @userRepository.findById(#id).get().getEmail() == authentication.getName()
+                 || authentication.getName() == 'hexlet@example.com'
+            """;
+
     @Autowired
     private UserRepository userRepository;
 
@@ -51,6 +66,7 @@ public class UsersController {
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize(ONLY_OWNER_OR_ADMIN)
     public UserDTO update(@RequestBody UserUpdateDTO userData, @PathVariable Long id) {
         var user = userRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Not Found"));
@@ -70,6 +86,7 @@ public class UsersController {
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PreAuthorize(ONLY_OWNER_OR_ADMIN)
     public void delete(@PathVariable Long id) {
         var user = userRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Not Found"));
