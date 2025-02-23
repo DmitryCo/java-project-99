5c1e555fabf1c5b786fa51e823c9d3edfe207362
Dmitry Konstantinov <dm3ypost@gmail.com>
2025-02-23 17:26:29 +0300

    Add /mapper/TaskMapper

--- src/main/java/hexlet/code/mapper/TaskMapper.java
+++ src/main/java/hexlet/code/mapper/TaskMapper.java
@@ -1,2 +1,98 @@
-package hexlet.code.mapper;public class TaskMapper {
+package hexlet.code.mapper;
+
+import hexlet.code.dto.task.TaskCreateDTO;
+import hexlet.code.dto.task.TaskDTO;
+import hexlet.code.dto.task.TaskUpdateDTO;
+import hexlet.code.exception.ResourceNotFoundException;
+import hexlet.code.model.Label;
+import hexlet.code.model.Task;
+import hexlet.code.model.TaskStatus;
+import hexlet.code.model.User;
+import hexlet.code.repository.LabelsRepository;
+import hexlet.code.repository.TaskStatusRepository;
+import hexlet.code.repository.UserRepository;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+import org.mapstruct.Named;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Mapper(
+        uses = {JsonNullableMapper.class, ReferenceMapper.class},
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+public abstract class TaskMapper {
+    @Autowired
+    private TaskStatusRepository taskStatusRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private LabelsRepository labelsRepository;
+
+    @Mapping(target = "name", source = "title")
+    @Mapping(target = "description", source = "content")
+    @Mapping(target = "assignee", source = "assigneeId", qualifiedByName = "idToAssignee")
+    @Mapping(target = "taskStatus", source = "status", qualifiedByName = "slugToTaskStatus")
+    @Mapping(target = "labels", source = "taskLabelIds", qualifiedByName = "idsToLabels")
+    public abstract Task map(TaskCreateDTO taskCreateDTO);
+
+    @Mapping(source = "name", target = "title")
+    @Mapping(source = "description", target = "content")
+    @Mapping(source = "assignee.id", target = "assigneeId")
+    @Mapping(source = "taskStatus.slug", target = "status")
+    @Mapping(source = "labels", target = "taskLabelIds", qualifiedByName = "labelsToIds")
+    public abstract TaskDTO map(Task task);
+
+
+    @Mapping(target = "name", source = "title")
+    @Mapping(target = "description", source = "content")
+    @Mapping(target = "assignee", source = "assigneeId", qualifiedByName = "idToAssignee")
+    @Mapping(target = "taskStatus", source = "status", qualifiedByName = "slugToTaskStatus")
+    @Mapping(target = "labels", source = "taskLabelIds", qualifiedByName = "idsToLabels")
+    public abstract void update(TaskUpdateDTO taskUpdateDTO, @MappingTarget Task task);
+
+
+    @Named("slugToTaskStatus")
+    public TaskStatus slugToTaskStatus(String slug) {
+        var taskStatus = taskStatusRepository.findBySlug(slug)
+                .orElseThrow(() -> new ResourceNotFoundException("TaskStatus with slug " + slug + " not found"));
+        return taskStatus;
+    }
+    @Named("idToAssignee")
+    public User idToAssignee(Long assigneeId) {
+        var assignee = userRepository.findById(assigneeId)
+                .orElseThrow(() -> new ResourceNotFoundException("User with id " + assigneeId + " not found"));
+        return assignee;
+    }
+
+    @Named("idsToLabels")
+    Set<Label> idsToLabels(Set<Long> ids) {
+        if (ids == null) {
+            return null;
+        }
+        return ids.stream()
+                .map(id -> {
+                    Label label = new Label();
+                    label.setId(id);
+                    return label;
+                }).collect(Collectors.toSet());
+    }
+
+    @Named("labelsToIds")
+    Set<Long> labelsToIds(Set<Label> labels) {
+        return labels == null ? null : labels.stream()
+                .map(Label::getId)
+                .collect(Collectors.toSet());
+    }
 }
