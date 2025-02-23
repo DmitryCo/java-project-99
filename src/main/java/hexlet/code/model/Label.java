02376f6d0d31ef3fb70f2911e5919c60dd1ca6a2
Dmitry Konstantinov <dm3ypost@gmail.com>
2025-02-23 17:52:12 +0300

    Add /model/Label

--- src/main/java/hexlet/code/model/Label.java
+++ src/main/java/hexlet/code/model/Label.java
@@ -1,2 +1,49 @@
-package hexlet.code.model;public class Label {
+package hexlet.code.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Column;
+import jakarta.persistence.CascadeType;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
+@ToString(includeFieldNames = true, onlyExplicitlyIncluded = true)
+@Table(name = "labels")
+@EqualsAndHashCode(of = {"name"})
+public class Label implements BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true)
+    @NotNull
+    @Size(min = 3, max = 1000)
+    private String name;
+
+    @CreatedDate
+    private LocalDate createdAt;
+
+    @ManyToMany(mappedBy = "labels", cascade = CascadeType.MERGE)
+    private Set<Task> tasks = new HashSet<>();
 }
