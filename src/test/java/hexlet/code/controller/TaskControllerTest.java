cf67c5aa8504da827d6fbc42662e319002fb55c4
Dmitry Konstantinov <dm3ypost@gmail.com>
2025-02-23 17:29:00 +0300

    Add /controller/TaskControllerTest

--- src/test/java/hexlet/code/controller/TaskControllerTest.java
+++ src/test/java/hexlet/code/controller/TaskControllerTest.java
@@ -1,2 +1,153 @@
-package hexlet.code.controller;public class TaskControllerTest {
+package hexlet.code.controller;
+
+import hexlet.code.util.ModelGenerator;
+import hexlet.code.dto.task.TaskCreateDTO;
+import hexlet.code.mapper.TaskMapper;
+import hexlet.code.model.Task;
+import hexlet.code.model.TaskStatus;
+import hexlet.code.model.User;
+import hexlet.code.repository.TaskRepository;
+import hexlet.code.repository.TaskStatusRepository;
+import hexlet.code.repository.UserRepository;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.instancio.Instancio;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.JwtRequestPostProcessor;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.nio.charset.StandardCharsets;
+
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class TaskControllerTest {
+    @Autowired
+    private WebApplicationContext wac;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper om;
+
+    @Autowired
+    private ModelGenerator modelGenerator;
+
+    private Task taskTest;
+
+    private JwtRequestPostProcessor token;
+    private User testUser;
+    private Task testTask;
+    private TaskStatus testStatus;
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private TaskMapper taskMapper;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private TaskStatusRepository taskStatusRepository;
+
+    @BeforeEach
+    public void clean() {
+        taskRepository.deleteAll();
+        userRepository.deleteAll();
+        taskStatusRepository.deleteAll();
+    }
+
+    @BeforeEach
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac)
+                .defaultResponseCharacterEncoding(StandardCharsets.UTF_8)
+                .apply(springSecurity())
+                .build();
+
+        testUser = Instancio.of(modelGenerator.getUserModel()).create();
+        token = jwt().jwt(builder -> builder.subject(testUser.getEmail()));
+        testTask = Instancio.of(modelGenerator.getTaskModel()).create();
+        testStatus = Instancio.of(modelGenerator.getTaskStatusModel()).create();
+        taskRepository.save(testTask);
+        userRepository.save(testUser);
+        taskStatusRepository.save(testStatus);
+    }
+
+
+    @Test
+    public void testShow() throws Exception {
+
+        var request = get("/api/tasks/{id}", testTask.getId()).with(token);
+
+        var result = mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andReturn();
+    }
+
+    @Test
+    public void testIndex() throws Exception {
+
+        var result = mockMvc.perform(get("/api/tasks").with(token))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        var body = result.getResponse().getContentAsString();
+
+        assertThatJson(body).isArray();
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+        var dto = new TaskCreateDTO();
+        Task task = Instancio.of(modelGenerator.getTaskModel()).create();
+
+        dto.setTitle(task.getName());
+        dto.setContent(task.getDescription());
+        dto.setIndex(task.getIndex());
+        dto.setAssigneeId(testUser.getId());
+        dto.setStatus(testStatus.getSlug());
+
+        var request = post("/api/tasks")
+                .with(token)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(om.writeValueAsString(dto));
+
+        mockMvc.perform(request)
+                .andExpect(status().isCreated());
+
+        var savedTask = taskRepository.findByName(dto.getTitle()).orElseThrow();
+
+        assertThat(savedTask.getName()).isEqualTo(dto.getTitle());
+        assertThat(savedTask.getDescription()).isEqualTo(dto.getContent());
+        assertThat(savedTask.getIndex()).isEqualTo(dto.getIndex());
+        assertThat(savedTask.getTaskStatus().getSlug()).isEqualTo(dto.getStatus());
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+
+        var request = delete("/api/tasks/{id}", testTask.getId()).with(token);
+
+        mockMvc.perform(request)
+                .andExpect(status().isNoContent());
+
+        assertFalse(taskRepository.findById(testTask.getId()).isPresent());
+    }
 }
