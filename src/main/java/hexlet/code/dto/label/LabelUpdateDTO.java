3824b68354677dc0b62f617c122175b4a36fc8de
Dmitry Konstantinov <dm3ypost@gmail.com>
2025-02-23 17:53:20 +0300

    Add /label/LabelCreateDTO, LabelDTO, LabelUpdateDTO

--- src/main/java/hexlet/code/dto/label/LabelUpdateDTO.java
+++ src/main/java/hexlet/code/dto/label/LabelUpdateDTO.java
@@ -1,2 +1,16 @@
-package hexlet.code.dto.label;public class LabelUpdateDTO {
+package hexlet.code.dto.label;
+
+import jakarta.validation.constraints.NotNull;
+
+import org.openapitools.jackson.nullable.JsonNullable;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LabelUpdateDTO {
+
+    @NotNull
+    private JsonNullable<String> name;
 }
