0da517bf08089cd2c044ffcfcdd9788c7f6fa388
Dmitry Konstantinov <dm3ypost@gmail.com>
2025-02-23 17:27:03 +0300

    Add /repository/TaskRepository

--- src/main/java/hexlet/code/repository/TaskRepository.java
+++ src/main/java/hexlet/code/repository/TaskRepository.java
@@ -1,2 +1,12 @@
-package hexlet.code.repository;public class TaskRepository {
+package hexlet.code.repository;
+
+import hexlet.code.model.Task;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.Optional;
+
+public interface TaskRepository extends JpaRepository<Task, Long>, JpaSpecificationExecutor<Task> {
+    Optional<Task> findByName(String name);
 }
