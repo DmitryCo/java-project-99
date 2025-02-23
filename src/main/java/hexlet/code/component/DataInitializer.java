def6f455f4cd54553b3104116096b0071d3bae6c
Dmitry Konstantinov <dm3ypost@gmail.com>
2025-02-23 13:53:20 +0300

    Update /component/DataInitializer, RsaKeyProperties

--- src/main/java/hexlet/code/component/DataInitializer.java
+++ src/main/java/hexlet/code/component/DataInitializer.java
@@ -1,12 +1,12 @@
 package hexlet.code.component;
 
+import hexlet.code.controller.api.UsersController;
+import hexlet.code.dto.user.UserCreateDTO;
 import hexlet.code.service.CustomUserDetailsService;
-import hexlet.code.model.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import lombok.AllArgsConstructor;
@@ -18,14 +18,17 @@ public class DataInitializer implements ApplicationRunner {
     @Autowired
     private final CustomUserDetailsService userService;
 
+    @Autowired
+    private final UsersController usersController;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        var userData = new User();
-        var email = "hexlet@example.com";
-        userData.setEmail(email);
-        userData.setFirstName("Admin");
-        userData.setLastName("Admin");
-        userData.setPasswordDigest("qwerty");
-        userService.createUser((UserDetails) userData);
+        var admin = new UserCreateDTO();
+
+        admin.setEmail("hexlet@example.com");
+        admin.setFirstName("Admin");
+        admin.setLastName("Admin");
+        admin.setPassword("qwerty");
+        usersController.create(admin);
     }
 }
