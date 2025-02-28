b7de200f76ea5712a3acc03fa30c3511d97deefc
Dmitry Konstantinov <dm3ypost@gmail.com>
2025-03-01 01:14:07 +0300

    Add testFilteredIndex()

--- src/test/java/hexlet/code/controller/TaskControllerTest.java
+++ src/test/java/hexlet/code/controller/TaskControllerTest.java
@@ -2,12 +2,13 @@ package hexlet.code.controller;
 
 import hexlet.code.util.ModelGenerator;
 import hexlet.code.dto.task.TaskCreateDTO;
-import hexlet.code.mapper.TaskMapper;
 import hexlet.code.model.Task;
+import hexlet.code.model.Label;
 import hexlet.code.model.TaskStatus;
 import hexlet.code.model.User;
 import hexlet.code.repository.TaskRepository;
 import hexlet.code.repository.TaskStatusRepository;
+import hexlet.code.repository.LabelsRepository;
 import hexlet.code.repository.UserRepository;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -16,6 +17,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -26,6 +28,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Set;
 
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -51,8 +54,6 @@ public class TaskControllerTest {
     @Autowired
     private ModelGenerator modelGenerator;
 
-    private Task taskTest;
-
     private JwtRequestPostProcessor token;
     private User testUser;
     private Task testTask;
@@ -61,9 +62,9 @@ public class TaskControllerTest {
     private TaskRepository taskRepository;
 
     @Autowired
-    private TaskMapper taskMapper;
-    @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private LabelsRepository labelsRepository;
     @Autowired
     private TaskStatusRepository taskStatusRepository;
 
@@ -111,6 +112,41 @@ public class TaskControllerTest {
         var body = result.getResponse().getContentAsString();
 
         assertThatJson(body).isArray();
+    }
+
+    @Test
+    public void testFilteredIndex() throws Exception {
+        var user = Instancio.of(modelGenerator.getUserModel())
+                .create();
+        userRepository.save(user);
+
+        var status = new TaskStatus();
+        status.setName("TestStatus");
+        status.setSlug("forTest");
+        taskStatusRepository.save(status);
+
+        var label = new Label();
+        label.setName("testLabel");
+        labelsRepository.save(label);
+
+        var task = new Task();
+        task.setName("TaskName");
+        task.setAssignee(user);
+        task.setTaskStatus(status);
+        task.setLabels(Set.of(label));
+        taskRepository.save(task);
+
+        var result = mockMvc.perform(get(
+                        "/api/tasks?titleCont=TaskName&assigneeId={id}&status=forTest&labelId={labId}",
+                        user.getId(), label.getId())
+                        .with(token))
+                .andExpect(status().isOk())
+                .andReturn();
+        var body = result.getResponse().getContentAsString();
+
+        assertTrue(body.contains(String.valueOf(user.getId())));
+        assertTrue(body.contains(status.getSlug()));
+        assertTrue(body.contains(String.valueOf(label.getId())));
     }
 
     @Test
